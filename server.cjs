98adde3fd08ee559ba68c1d7d6fd9c79afe8a25f
Karthik Jayaraman <kjram@Karthiks-MacBook-Pro.local>
2025-03-15 10:02:04 +0530

    jtxviewer render

--- server.cjs
+++ server.cjs
@@ -32,7 +32,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://jtxviewer.onrender.com:5173",
     methods: ["GET", "POST"],
     credentials: true
   },
@@ -48,7 +48,7 @@ io.on('connection', (socket) => {
 });
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://jtxviewer.onrender.com:5173',
   methods: ['GET', 'POST'],
   credentials: true
 }));
